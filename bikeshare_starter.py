9c37ff170ff561bb77b66cf223b3f45dcb3e0405
Onur Oezcelik <onur.oezcelik@cariad.technology>
2026-03-10 09:56:18 +0100

    refactor: clarify print messages for readability in time_stats

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -48,7 +48,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCalculating most frequent travel times...\n')
     start_time = time.time()
 
     # display the most common month
